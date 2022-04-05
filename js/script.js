49fda3553f603adf1030fe899887ec86fbd5f4bc
kit_sullivan <kitsullivan@gmail.com>
2022-04-05 00:38:32 -0400

    Added css and js files

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var modal = document.querySelector("#bioModal");
+
+var bandName = document.querySelector(".bandName");
+
+var closeBtn = document.querySelector(".close");
+
+//open modal on click of band name
+bandName.onclick = function() {
+	modal.style.display = "block";
+}
+
+
+
+
