3b3e1436f601fea38941240850f77f4ef9fec8b7
Kit Sullivan <kitsullivan@gmail.com>
2022-04-05 11:10:21 -0400

    update js with close function

--- js/script.js
+++ js/script.js
@@ -5,10 +5,18 @@ var bandName = document.querySelector(".bandName");
 var closeBtn = document.querySelector(".close");
 
 //open modal on click of band name
-bandName.onclick = function() {
-	modal.style.display = "block";
-}
+bandName.onclick = function () {
+  modal.style.display = "block";
+};
 
+//close modal on click of X
+closeBtn.onclick = function () {
+  modal.style.display = "none";
+};
 
-
-
+//close modal on click outside of modal
+window.onclick = function (event) {
+  if (event.target !== modal) {
+    modal.style.display = "none";
+  }
+};
