edcec7d31d463061a731699bbea6e2dbfaf2b18e
kit_sullivan <kitsullivan@gmail.com>
2022-04-05 11:13:00 -0400

    updates to make close button work, removal of unneeded files

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var modal = document.querySelector("#bioModal");
+var bioModal = document.querySelector("#bioModal");
 
 var bandName = document.querySelector(".bandName");
 
@@ -6,9 +6,18 @@ var closeBtn = document.querySelector(".close");
 
 //open modal on click of band name
 bandName.onclick = function() {
-	modal.style.display = "block";
+	bioModal.style.display = "block";
 }
 
+//close modal on click of X
+closeBtn.onclick = function() {
+	bioModal.style.display = 'none';
+}
 
-
+//close modal on click outside of modal
+window.onclick = function(event) {
+	if (event.target == bioModal) {
+		bioModal.style.display = 'none';
+	}
+}
 
